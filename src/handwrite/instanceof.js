d2907bb68cd2a5f40478b3b548d5c9d85aed4a13
bowlofnoodles <huahuazfh@qq.com>
2021-01-16 12:18:50 +0800

    fix: instanceof 增加基础类型的判断

--- src/handwrite/instanceof.js
+++ src/handwrite/instanceof.js
@@ -1,4 +1,8 @@
+// instanceof 运算符用于检测构造函数的 prototype 属性是否出现在某个实例对象的原型链上。
+const isObject = target => typeof target === 'object' && target !== 'null';
+const isFunction = target => typeof target === 'function';
 function instanceOf(target, origin) {
+  if (!(isObject || isFunction)) return false;
   if (!target.__proto__) return false;
   while(target.__proto__) {
     const proto = target.__proto__;
