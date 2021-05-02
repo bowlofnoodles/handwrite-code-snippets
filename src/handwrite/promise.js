88c6864fdb17737f4126d036b891ce8794db7dae
bowlofnoodles <huahuazfh@qq.com>
2021-05-02 17:09:43 +0800

    refactor: promise

--- src/handwrite/promise.js
+++ src/handwrite/promise.js
@@ -1,35 +1,46 @@
-const PENDING = 'pending';
-const FULFILLED = 'fulfilled';
-const REJECTED = 'rejected';
-
-// npm i -g promises-aplus-tests
-// promises-aplus-tests promise.js
-
 function Promise(executor) {
-  this.state = 'pending';
-  this.value = null;
-  this.reason = null;
-  this.onFulfilledCallback = [];
-  this.onRejectedCallback = [];
-  
-  const resolve = value => {
-    if (this.state === PENDING) {
-      this.state = FULFILLED;
-      this.value = value;
-      this.onFulfilledCallback.forEach(fn => {
-        fn();
-      });
+  var self = this;
+
+  self.status = 'pending';
+
+  self.onResolvedCallback = [];
+
+  self.onRejectedCallback = [];
+
+  function resolve(value) {
+    if (value instanceof Promise) {
+      return value.then(resolve, reject);
     }
+
+    setTimeout(function () {
+      // 异步执行所有的回调函数
+
+      if (self.status === 'pending') {
+        self.status = 'resolved';
+
+        self.data = value;
+
+        for (var i = 0; i < self.onResolvedCallback.length; i++) {
+          self.onResolvedCallback[i](value);
+        }
+      }
+    });
   }
 
-  const reject = reason => {
-    if (this.state === PENDING) {
-      this.state = REJECTED;
-      this.reason = reason;
-      this.onRejectedCallback.forEach(fn => {
-        fn()
-      });
-    }
+  function reject(reason) {
+    setTimeout(function () {
+      // 异步执行所有的回调函数
+
+      if (self.status === 'pending') {
+        self.status = 'rejected';
+
+        self.data = reason;
+
+        for (var i = 0; i < self.onRejectedCallback.length; i++) {
+          self.onRejectedCallback[i](reason);
+        }
+      }
+    });
   }
 
   try {
@@ -37,44 +48,160 @@ function Promise(executor) {
   } catch (reason) {
     reject(reason);
   }
-}
+};
 
-Promise.prototype.then = function(onFulfilled, onRejected) {
-  const onFulfilledFunc = typeof onFulfilled === 'function' ? onFulfilled : value => value;
-  const onRejectedFunc = typeof onRejected === 'function' ? onRejected : reason => reason;
-  switch (this.state) {
-    case FULFILLED:
-      setTimeout(() => {
-        onFulfilledFunc(this.value);
-      });
-      break;
-    case REJECTED:
-      setTimeout(() => {
-        onRejectedFunc(this.reason);
-      });
-    case PENDING:
-      this.onFulfilledCallback.push(() => {
-        setTimeout(() => {
-          onFulfilledFunc(this.value);
-        });
-      });
-      this.onRejectedCallback.push(() => {
-        setTimeout(() => {
-          onRejectedFunc(this.reason);
-        })
-      });
-    default:
-      break;
+function resolvePromise(promise2, x, resolve, reject) {
+  var then;
+
+  var thenCalledOrThrow = false;
+
+  if (promise2 === x) {
+    return reject(new TypeError('Chaining cycle detected for promise!'));
+  }
+
+  if (x instanceof Promise) {
+    if (x.status === 'pending') {
+      x.then(function (v) {
+        resolvePromise(promise2, v, resolve, reject);
+      }, reject);
+    } else {
+      x.then(resolve, reject);
+    }
+
+    return;
+  }
+
+  if (x !== null && (typeof x === 'object' || typeof x === 'function')) {
+    try {
+      then = x.then;
+
+      if (typeof then === 'function') {
+        then.call(
+          x,
+          function rs(y) {
+            if (thenCalledOrThrow) return;
+
+            thenCalledOrThrow = true;
+
+            return resolvePromise(promise2, y, resolve, reject);
+          },
+          function rj(r) {
+            if (thenCalledOrThrow) return;
+
+            thenCalledOrThrow = true;
+
+            return reject(r);
+          }
+        );
+      } else {
+        resolve(x);
+      }
+    } catch (e) {
+      if (thenCalledOrThrow) return;
+
+      thenCalledOrThrow = true;
+
+      return reject(e);
+    }
+  } else {
+    resolve(x);
   }
 };
 
-Promise.defer = Promise.deferred = function () {
-  let dfd = {};
-  dfd.promise = new Promise((resolve,reject)=>{
-    dfd.resolve = resolve;
-    dfd.reject = reject;
-  })
-  return dfd;
-}
+Promise.prototype.then = function (onResolved, onRejected) {
+  var self = this;
+
+  var promise2;
+
+  onResolved =
+    typeof onResolved === 'function'
+      ? onResolved
+      : function (v) {
+          return v;
+        };
+
+  onRejected =
+    typeof onRejected === 'function'
+      ? onRejected
+      : function (r) {
+          throw r;
+        };
+
+  if (self.status === 'resolved') {
+    return (promise2 = new Promise(function (resolve, reject) {
+      setTimeout(function () {
+        // 异步执行onResolved
+
+        try {
+          var x = onResolved(self.data);
+
+          resolvePromise(promise2, x, resolve, reject);
+        } catch (reason) {
+          reject(reason);
+        }
+      });
+    }));
+  }
+
+  if (self.status === 'rejected') {
+    return (promise2 = new Promise(function (resolve, reject) {
+      setTimeout(function () {
+        // 异步执行onRejected
+
+        try {
+          var x = onRejected(self.data);
+
+          resolvePromise(promise2, x, resolve, reject);
+        } catch (reason) {
+          reject(reason);
+        }
+      });
+    }));
+  }
+
+  if (self.status === 'pending') {
+    // 这里之所以没有异步执行，是因为这些函数必然会被resolve或reject调用，而resolve或reject函数里的内容已是异步执行，构造函数里的定义
+
+    return (promise2 = new Promise(function (resolve, reject) {
+      self.onResolvedCallback.push(function (value) {
+        try {
+          var x = onResolved(value);
+
+          resolvePromise(promise2, x, resolve, reject);
+        } catch (r) {
+          reject(r);
+        }
+      });
+
+      self.onRejectedCallback.push(function (reason) {
+        try {
+          var x = onRejected(reason);
+
+          resolvePromise(promise2, x, resolve, reject);
+        } catch (r) {
+          reject(r);
+        }
+      });
+    }));
+  }
+};
+
+Promise.prototype.catch = function (onRejected) {
+  return this.then(null, onRejected);
+};
 
 module.exports = Promise;
+
+// 最后这个是测试用的，后面会说
+
+Promise.deferred = Promise.defer = function () {
+  var dfd = {};
+
+  dfd.promise = new Promise(function (resolve, reject) {
+    dfd.resolve = resolve;
+
+    dfd.reject = reject;
+  });
+
+  return dfd;
+};
