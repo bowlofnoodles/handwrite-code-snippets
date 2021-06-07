09080f8c936960652addc5f41b51df81aa4c3718
bowlofnoodles <huahuazfh@qq.com>
2021-06-07 20:08:29 +0800

    feat: 手写promisepromise

--- src/handwrite/promise.js
+++ src/handwrite/promise.js
@@ -2,46 +2,33 @@ const PENDING = 'pending';
 const FULFILLED = 'fulfilled';
 const REJECTED = 'rejected';
 
-function Promise(executor) {
-  var self = this;
-
-  self.status = PENDING;
-
-  self.onResolvedCallback = [];
-
-  self.onRejectedCallback = [];
+function MyPromise(executor) {
+  this.status = PENDING;
+  this.value = void 0;
+  this.reason = void 0;
+  this.onResolvedCallback = [];
+  this.onRejectedCallback = [];
+  const ctx = this;
 
   function resolve(value) {
-    if (value instanceof Promise) {
-      return value.then(resolve, reject);
-    }
-
-    setTimeout(function () {
-      // 异步执行所有的回调函数
-
-      if (self.status === PENDING) {
-        self.status = FULFILLED;
-
-        self.data = value;
-
-        for (var i = 0; i < self.onResolvedCallback.length; i++) {
-          self.onResolvedCallback[i](value);
+    queueMicrotask(() => {
+      if (ctx.status === PENDING) {
+        ctx.status = FULFILLED;
+        ctx.value = value;
+        for (let i = 0; i < ctx.onResolvedCallback.length; i ++) {
+          ctx.onResolvedCallback[i](ctx.value);
         }
       }
     });
   }
 
   function reject(reason) {
-    setTimeout(function () {
-      // 异步执行所有的回调函数
-
-      if (self.status === PENDING) {
-        self.status = REJECTED;
-
-        self.data = reason;
-
-        for (var i = 0; i < self.onRejectedCallback.length; i++) {
-          self.onRejectedCallback[i](reason);
+    queueMicrotask(() => {
+      if (ctx.status === PENDING) {
+        ctx.status = REJECTED;
+        ctx.reason = reason;
+        for (let i = 0; i < ctx.onRejectedCallback.length; i++) {
+          ctx.onRejectedCallback[i](ctx.reason);
         }
       }
     });
@@ -49,163 +36,115 @@ function Promise(executor) {
 
   try {
     executor(resolve, reject);
-  } catch (reason) {
-    reject(reason);
+  } catch (err) {
+    reject(err);
   }
-}
+};
 
-function resolvePromise(promise2, x, resolve, reject) {
-  var then;
-
-  var thenCalledOrThrow = false;
-
-  if (promise2 === x) {
-    return reject(new TypeError('Chaining cycle detected for promise!'));
-  }
-
-  if (x instanceof Promise) {
+function resolvePromise(promise, x, resolve, reject) {
+  if (promise === x) return reject(new TypeError('Cycle Chain Detected in promise'));
+  if (x instanceof MyPromise) {
     if (x.status === PENDING) {
-      x.then(function (v) {
-        resolvePromise(promise2, v, resolve, reject);
-      }, reject);
+      x.then(v => resolvePromise(promise, v, resolve, reject), reject);
     } else {
       x.then(resolve, reject);
     }
-
     return;
   }
-
-  if (x !== null && (typeof x === 'object' || typeof x === 'function')) {
+  let thenOrThrowCalled = false;
+  if ((x !== null && typeof x === 'object') || typeof x === 'function') {
     try {
-      then = x.then;
-
+      let then = x.then;
       if (typeof then === 'function') {
         then.call(
           x,
           function rs(y) {
-            if (thenCalledOrThrow) return;
-
-            thenCalledOrThrow = true;
-
-            return resolvePromise(promise2, y, resolve, reject);
+            if (thenOrThrowCalled) return;
+            thenOrThrowCalled = true;
+            return resolvePromise(promise, y, resolve, reject);
           },
-          function rj(r) {
-            if (thenCalledOrThrow) return;
-
-            thenCalledOrThrow = true;
-
-            return reject(r);
+          function rj(e) {
+            if (thenOrThrowCalled) return;
+            thenOrThrowCalled = true;
+            return reject(e);
           }
-        );
+        )
       } else {
         resolve(x);
       }
-    } catch (e) {
-      if (thenCalledOrThrow) return;
-
-      thenCalledOrThrow = true;
-
-      return reject(e);
+    } catch (err) {
+      if (thenOrThrowCalled) return;
+      thenOrThrowCalled = true;
+      reject(err);
     }
   } else {
     resolve(x);
   }
 }
 
-Promise.prototype.then = function (onResolved, onRejected) {
-  var self = this;
+MyPromise.prototype.then = function(onFulfilled, onRejected) {
+  const ctx = this;
+  onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : value => value;
+  onRejected = typeof onRejected === 'function' ? onRejected : reason => {throw reason};
 
-  var promise2;
-
-  onResolved =
-    typeof onResolved === 'function'
-      ? onResolved
-      : function (v) {
-          return v;
-        };
-
-  onRejected =
-    typeof onRejected === 'function'
-      ? onRejected
-      : function (r) {
-          throw r;
-        };
-
-  if (self.status === FULFILLED) {
-    return (promise2 = new Promise(function (resolve, reject) {
-      setTimeout(function () {
-        // 异步执行onResolved
-
+  if (ctx.status === PENDING) {
+    let promise2 = new MyPromise((resolve, reject) => {
+      ctx.onResolvedCallback.push(value => {
         try {
-          var x = onResolved(self.data);
-
-          resolvePromise(promise2, x, resolve, reject);
-        } catch (reason) {
-          reject(reason);
+          let x = onFulfilled(value);
+          resolvePromise(promise2, x, resolve, reject); 
+        } catch (err) {
+          reject(err);
         }
       });
-    }));
+      ctx.onRejectedCallback.push(reason => {
+        try {
+          let x = onRejected(reason);
+          resolvePromise(promise2, x, resolve, reject);
+        } catch (err) {
+          reject(err);
+        }
+      });
+    });
+    return promise2;
   }
 
-  if (self.status === REJECTED) {
-    return (promise2 = new Promise(function (resolve, reject) {
-      setTimeout(function () {
-        // 异步执行onRejected
-
+  if (ctx.status === FULFILLED) {
+    let promise2 = new MyPromise((resolve, reject) => {
+      queueMicrotask(() => {
         try {
-          var x = onRejected(self.data);
-
+          let x = onFulfilled(ctx.value);
           resolvePromise(promise2, x, resolve, reject);
-        } catch (reason) {
-          reject(reason);
+        } catch (err) {
+          reject(err);
         }
-      });
-    }));
+      })
+    });
+    return promise2;
   }
 
-  if (self.status === PENDING) {
-    // 这里之所以没有异步执行，是因为这些函数必然会被resolve或reject调用，而resolve或reject函数里的内容已是异步执行，构造函数里的定义
-
-    return (promise2 = new Promise(function (resolve, reject) {
-      self.onResolvedCallback.push(function (value) {
+  if (ctx.status === REJECTED) {
+    let promise2 = new MyPromise((resolve, reject) => {
+      queueMicrotask(() => {
         try {
-          var x = onResolved(value);
-
+          let x = onRejected(ctx.reason);
           resolvePromise(promise2, x, resolve, reject);
-        } catch (r) {
-          reject(r);
+        } catch (err) {
+          reject(err);
         }
       });
-
-      self.onRejectedCallback.push(function (reason) {
-        try {
-          var x = onRejected(reason);
-
-          resolvePromise(promise2, x, resolve, reject);
-        } catch (r) {
-          reject(r);
-        }
-      });
-    }));
+    });
+    return promise2;
   }
 };
 
-Promise.prototype.catch = function (onRejected) {
-  return this.then(null, onRejected);
-};
+module.exports = MyPromise;
 
-export default Promise;
-
-// 最后这个是测试用的，后面会说
-
-Promise.deferred = Promise.defer = function () {
-  var dfd = {};
-
-  dfd.promise = new Promise(function (resolve, reject) {
+MyPromise.defer = MyPromise.deferred = function () {
+  let dfd = {};
+  dfd.promise = new MyPromise((resolve, reject) => {
     dfd.resolve = resolve;
-
     dfd.reject = reject;
   });
-
   return dfd;
 };
