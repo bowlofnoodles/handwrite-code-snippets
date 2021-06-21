3145a2374ffa1d5b96ba4fd4aa7e6f8604aa437e
bowlofnoodles <huahuazfh@qq.com>
2021-06-21 19:23:06 +0800

    refactor: 零散更改

--- src/handwrite/array.js
+++ src/handwrite/array.js
@@ -1,7 +1,13 @@
 Array.prototype.reduce = function(cb, init) {
   if (typeof cb !== 'function') throw new TypeError(`${cb} is not a function`);
-  if (this.length <= 0) throw new TypeError('reduce of empty array with no initial value');
   const hasInit = arguments.length >= 2;
+  if (this.length <= 0) {
+    if (!hasInit) {
+      throw new TypeError('reduce of empty array with no initial value'); 
+    } else {
+      return init;
+    }
+  }
   init = hasInit ? init : this[0];
   for (let i = hasInit ? 0 : 1; i < this.length; i ++) {
     init = cb(init, this[i], i, this);
@@ -16,25 +22,25 @@ Array.prototype.filter = function(cb, thisArg) {
 
 Array.prototype.map = function(cb, thisArg) {
   if (typeof cb !== 'function') throw new TypeError(`${cb} is not a function`);
-  return this.reduce((c, n, i) => c.concat(cb(n, i, this)), []);
+  return this.reduce((c, n, i) => c.concat(cb.call(thisArg, n, i, this)), []);
 }
 
 Array.prototype.find = function(cb, thisArg) {
   if (typeof cb !== 'function') throw new TypeError(`${cb} is not a function`);
-  return this.reduce((c, n, i) => (cb(n, i, this) && c === undefined) ? n : c, undefined);
+  return this.reduce((c, n, i) => (cb.call(thisArg, n, i, this) && c === undefined) ? n : c, undefined);
 }
 
 Array.prototype.findIndex = function(cb, thisArg) {
   if (typeof cb !== 'function') throw new TypeError(`${cb} is not a function`);
-  return this.reduce((c, n, i) => (cb(n, i, this) && c === -1) ? i : c, -1);
+  return this.reduce((c, n, i) => (cb.call(thisArg, n, i, this) && c === -1) ? i : c, -1);
 }
 
 Array.prototype.some = function(cb) {
   if (typeof cb !== 'function') throw new TypeError(`${cb} is not a function`);
-  return this.reduce((c, n, i) => cb(n, i, this) ? true : c, false);
+  return this.reduce((c, n, i) => cb.call(thisArg, n, i, this) ? true : c, false);
 }
 
 Array.prototype.every = function(cb) {
   if (typeof cb !== 'function') throw new TypeError(`${cb} is not a function`);
-  return this.reduce((c, n, i) => cb(n, i, this) && c, true);
+  return this.reduce((c, n, i) => cb.call(thisArg, n, i, this) && c, true);
 }
--- src/koa/index.js
+++ src/koa/index.js
@@ -1,21 +1,21 @@
 // // 2.koa compose
-// const middlewares = [
-//   async (ctx, next) => {
-//     console.log(1);
-//     await next();
-//     console.log(2);
-//   },
-//   async (ctx, next) => {
-//     console.log(3);
-//     await next();
-//     console.log(4);
-//   },
-//   async (ctx, next) => {
-//     console.log(5);
-//     await next();
-//     console.log(6);
-//   },
-// ];
+const middlewares = [
+  async (ctx, next) => {
+    console.log(1);
+    await next();
+    console.log(2);
+  },
+  async (ctx, next) => {
+    console.log(3);
+    await next();
+    console.log(4);
+  },
+  async (ctx, next) => {
+    console.log(5);
+    await next();
+    console.log(6);
+  },
+];
 
 // const compose = middlewares => {
 //   if (!Array.isArray(middlewares)) throw new TypeError('middlewares必须为数组');
@@ -39,3 +39,26 @@
 // };
 
 // compose(middlewares)().then();
+
+function myCompose(middlewares) {
+  // 参数判错 要是数组 且 数组每个元素都要是函数 省略了...
+  return function (ctx, next) {
+    let index = -1;
+    return dispatch(0);
+    function dispatch(i) {
+      if (i <= index) return Promise.reject(new Error('单个中间件函数中多次调用了next函数'));
+      let fn = middlewares[i];
+      index = i;
+      // next就是最后一个中间件
+      if (i === middlewares.length) fn = next;
+      // 如果next不传，则代表结束了 直接resolve
+      if (!fn) return Promise.resolve();
+      try {
+        // 递归等待全部完成
+        return Promise.resolve(fn(ctx, dispatch.bind(null, i + 1)));
+      } catch (error) {
+        return Promise.reject(error);
+      }
+    }
+  };
+}
