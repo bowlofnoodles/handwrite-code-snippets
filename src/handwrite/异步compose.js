5ed161ec5e71b4cff87cda8868a8c7a2759e469d
bowlofnoodles <huahuazfh@qq.com>
2021-06-21 20:33:12 +0800

    refactor: commit

--- src/handwrite/异步compose.js
+++ src/handwrite/异步compose.js
@@ -31,21 +31,22 @@ const init = async (a, b) => {
   return result;
 };
 
-const compose =
-  (...funcs) =>
-  (...arg) =>
-    funcs.reduceRight((chain, func, i) => {
-      if (i === funcs.length - 1) {
-        return chain.then(res => {
-          // 入口函数可能有多参数，作特殊处理
-          return func(...res);
-        });
-      } else {
-        return chain.then(func);
-      }
-    }, Promise.resolve(arg));
-
 const composed = compose(stepAdd, stepMulti, init);
+composed(9, 10).then(res => {
+  console.log('res', res); // 40 => (9 + 10) * 2 + 2
+})
+
+const compose = (...funcs) => (...arg) => funcs.reduceRight((chain, func, i) => {
+  if (i === funcs.length - 1) {
+    return chain.then(res => {
+      // 入口函数可能有多参数，作特殊处理
+      return func(...res);
+    });
+  } else {
+    return chain.then(func);
+  }
+}, Promise.resolve(arg));
+
 
 // 隔一秒打印数字的另一种做法
 const getPromise = val =>
