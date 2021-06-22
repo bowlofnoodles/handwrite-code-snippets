6c83b79daba98e96c48bedde306869e13c4be562
bowlofnoodles <huahuazfh@qq.com>
2021-06-22 11:06:24 +0800

    refactor: curry

--- src/handwrite/curry.js
+++ src/handwrite/curry.js
@@ -1,26 +1,51 @@
-const curry = (fn, ...args) => {
+function curry(fn, ...args) {
   if (args.length >= fn.length) {
     return fn(...args);
-  } else {
-    return (...args2) => curry(fn, ...args, ...args2);
   }
+  return (...args2) => curry(fn, ...args, ...args2);
 }
 
-const add = (a, b, c) => a + b + c;
+function add(a, b, c) {
+  return a + b + c;
+}
 
-const curryAdd = curry(add);
+const curryAdd = curry(add, 1);
 
-curryAdd(1)(2)(3);
-curryAdd(1)(2, 3);
-curryAdd(1, 2)(3);
-curryAdd(1, 2, 3);
+console.log(curryAdd(1)(100), curryAdd(1, 10));
 
-const curryAddNotEnd = (...args) => {
-  const addSum = (arr, init) => arr.reduce((c, n) => c + n, init);
-  let sum = addSum(args, 0);
-  // 以不传参数为终止
-  const x = (...b) => {if (!b || !b.length) return sum; sum = addSum(b, sum); return x};
+function addNotEnd(...args) {
+  return args.reduce((c, n) => c + n, 0);
+}
+
+function curryNotEnd(fn, ...args) {
+  // 返回一个函数
+  return (...args2) => {
+    // 如果不传参数 则执行函数计算结果
+    if (args2.length === 0) {
+      return fn(...args);
+    } else {
+      return curryNotEnd(fn, ...args, ...args2);
+    }
+  };
+}
+
+const curryNotEndAdd = curryNotEnd(addNotEnd);
+
+console.log(curryNotEndAdd(1, 2, 3)(2, 4)(4)(10)()); // 26
+
+function curryNotEndAnother(fn, ...args) {
+  const x = (...args2) => {
+    // 拼接参数，以便toString能够拿到
+    args.push(...args2);
+    // 如果不传参数 则执行函数计算结果
+    return curryNotEndAnother(fn, ...args);
+  };
+  x.toString = function () {
+    return fn(...args);
+  };
   return x;
 }
 
-console.log('add', curryAddNotEnd(1, 2, 3)(2, 2, 3)(3)(4, 2, 20, 1234)(5)(6)(7)(8, 10, 2, 8)(9)(10)());
+const curryNotEndAddAnother = curryNotEndAnother(addNotEnd);
+
+console.log(curryNotEndAddAnother(1, 2, 3)(2, 4)(4)(10) + 10); // 26
